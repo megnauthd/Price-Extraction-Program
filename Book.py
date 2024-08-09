8d6120a72bd54654c37712f364933160161c5091
Megnauth Dhanraj <megnauthdhanraj@Megnauths-Air.lan>
2024-08-08 22:26:59 -0400

    Used beautifulSoup and accomplished phase 2

--- Book.py
+++ Book.py
@@ -1,56 +1,74 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import csv
 
-# URL of the book page to scrape
-book_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+# Function to extract book information
+def extract_book_info(book_url):
+    response = requests.get(book_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Send a GET request to the book page
-response = requests.get(book_url)
-soup = BeautifulSoup(response.content, 'html.parser')
+    # Extract the required information
+    product_page_url = book_url
+    upc = soup.find('th', text='UPC').find_next_sibling('td').text
+    book_title = soup.find('h1').text
+    price_including_tax = soup.find('th', text='Price (incl. tax)').find_next_sibling('td').text
+    price_excluding_tax = soup.find('th', text='Price (excl. tax)').find_next_sibling('td').text
+    quantity_available = soup.find('th', text='Availability').find_next_sibling('td').text.strip()
+    product_description = soup.find('div', id='product_description').find_next('p').text
+    category = soup.find('ul', class_='breadcrumb').find_all('li')[2].text.strip()
+    review_rating = soup.find('p', class_='star-rating')['class'][1]
+    image_url = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
 
-# Extract the required information
-product_page_url = book_url
-upc = soup.find('th', text='UPC').find_next_sibling('td').text
-book_title = soup.find('h1').text
-price_including_tax = soup.find('th', text='Price (incl. tax)').find_next_sibling('td').text
-price_excluding_tax = soup.find('th', text='Price (excl. tax)').find_next_sibling('td').text
-quantity_available = soup.find('th', text='Availability').find_next_sibling('td').text.strip()
-product_description = soup.find('div', id='product_description').find_next('p').text
-category = soup.find('ul', class_='breadcrumb').find_all('li')[2].text.strip()
-review_rating = soup.find('p', class_='star-rating')['class'][1]
-image_url = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
+    # Return the extracted information as a dictionary
+    return {
+        'product_page_url': product_page_url,
+        'universal_product_code (upc)': upc,
+        'book_title': book_title,
+        'price_including_tax': price_including_tax,
+        'price_excluding_tax': price_excluding_tax,
+        'quantity_available': quantity_available,
+        'product_description': product_description,
+        'category': category,
+        'review_rating': review_rating,
+        'image_url': image_url
+    }
 
-# Print the extracted information for debugging
-print(f'product_page_url: {product_page_url}')
-print(f'universal_product_code (upc): {upc}')
-print(f'book_title: {book_title}')
-print(f'price_including_tax: {price_including_tax}')
-print(f'price_excluding_tax: {price_excluding_tax}')
-print(f'quantity_available: {quantity_available}')
-print(f'product_description: {product_description}')
-print(f'category: {category}')
-print(f'review_rating: {review_rating}')
-print(f'image_url: {image_url}')
+# Function to get all book URLs in a category, handling pagination
+def get_book_urls(category_url):
+    book_urls = []
+    while category_url:
+        response = requests.get(category_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-# Create a dictionary with the extracted data
-data = {
-    'product_page_url': [product_page_url],
-    'universal_product_code (upc)': [upc],
-    'book_title': [book_title],
-    'price_including_tax': [price_including_tax],
-    'price_excluding_tax': [price_excluding_tax],
-    'quantity_available': [quantity_available],
-    'product_description': [product_description],
-    'category': [category],
-    'review_rating': [review_rating],
-    'image_url': [image_url]
-}
+        # Find all product page URLs on the current page
+        book_links = soup.find_all('h3')
+        for link in book_links:
+            book_urls.append('http://books.toscrape.com/catalogue/' + link.find('a')['href'].replace('../../../', ''))
 
-# Convert the dictionary to a pandas DataFrame
-df = pd.DataFrame(data)
+        # Check if there is a next page, if so, update category_url
+        next_button = soup.find('li', class_='next')
+        category_url = 'http://books.toscrape.com/catalogue/' + next_button.find('a')['href'] if next_button else None
 
-# Write the DataFrame to a CSV file
-df.to_csv('book_info.csv', index=False)
+    return book_urls
 
-print('Data has been written to book_info.csv')
+# Category URL (Replace this URL with any category URL you choose)
+category_url = 'http://books.toscrape.com/catalogue/category/books/poetry_23/index.html'
+
+# Get all book URLs in the chosen category
+book_urls = get_book_urls(category_url)
+
+# List to hold all extracted data
+all_books_data = []
+
+# Extract data for each book in the category
+for book_url in book_urls:
+    book_data = extract_book_info(book_url)
+    all_books_data.append(book_data)
+
+# Write the data to a CSV file
+with open('category_books_info.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=all_books_data[0].keys())
+    writer.writeheader()
+    writer.writerows(all_books_data)
+
+print(f'Data for category has been written to category_books_info.csv')
